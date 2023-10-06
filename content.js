8254cbf5827deadb30fce81ee7c8cf680b1b6301
vkidik <vladgro720@gmail.com>
2023-10-06 18:35:58 +1100

    upd

--- content.js
+++ content.js
@@ -1,17 +1,73 @@
-alert('Hello, world!');
+class MWVotify{
+    constructor(){
+        this.token = ''
 
-// content.js (код, который выполняется на веб-странице)
-const socket = io('ws://localhost:3000'); // Укажите URL вашего сервера
+        this.checkLogin()
+    }
 
-// Функция для отправки сообщения на сервер
-function sendMessage(message) {
-  socket.emit('message', message);
+    checkLogin(){
+        chrome.storage.local.get(["token"]).then(async tokenResult => {
+            await (()=> {
+                if(tokenResult.token != ""){
+                    this.token = tokenResult.token
+                } else{
+                    while(this.token == ''){
+                        this.token = prompt("Enter API from your account")
+                    }
+                }
+            })()
+
+            await this.getData(this.token)
+        })
+    }
+
+    async getData(token){
+        await chrome.storage.local.set({ token: token }).then(() => {
+            console.log("Token is set");
+        });
+        
+        this.musicPlayer(token)
+    }
+
+    musicPlayer(token){
+        window.onSpotifyWebPlaybackSDKReady = () => {
+            window.player = new Spotify.Player({
+                name: 'KOMP Player',
+                getOAuthToken: cb => { cb(token); },
+                volume: 0.5
+            });
+          
+            // Ready
+            player.addListener('ready', ({ device_id }) => {
+                console.log('Ready with Device ID', device_id);
+            });
+          
+            // Not Ready
+            player.addListener('not_ready', ({ device_id }) => {
+                console.log('Device ID has gone offline', device_id);
+            });
+          
+            player.addListener('initialization_error', ({ message }) => {
+                console.error(message);
+            });
+          
+            player.addListener('authentication_error', ({ message }) => {
+                console.error(message);
+            });
+          
+            player.addListener('account_error', ({ message }) => {
+                console.error(message);
+            });
+          
+            document.getElementById('togglePlay').onclick = function() {
+              player.togglePlay();
+            };
+          
+            player.connect();
+          }
+    }
 }
 
-// Пример использования
-sendMessage('Привет, сервер!');
-
-// Функция для обработки сообщений от сервера
-socket.on('message', (data) => {
-  alert(data); // Отображение сообщения через alert
-});
+document.addEventListener('DOMContentLoaded', ()=> {
+    window.MWVotify = new MWVotify()
+})
